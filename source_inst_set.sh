02ed45878e718ae677aaf657c45f8bdfdd22f6f2
SergSha <96518320+SergSha@users.noreply.github.com>
2022-02-15 13:54:09 +0300

    Update and rename main1_inst_set.sh to source_inst_set.sh

--- source_inst_set.sh
+++ source_inst_set.sh
@@ -14,10 +14,10 @@ systemctl stop firewalld
 yum -y install httpd
 
 # Add index.html to path /var/www/html/
-cp -f /root/main1/index.html /var/www/html/
+cp -f /root/source/index.html /var/www/html/
 
 # Add main1_conf_upd.sh to cron.daily
-cp -f /root/main1/main1_conf_upd.sh /etc/cron.daily/
+cp -f /root/source/source_conf_upd.sh /etc/cron.daily/
 
 # Start httpd
 systemctl start httpd
